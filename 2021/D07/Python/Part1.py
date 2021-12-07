80ffc9753ecdecb55ee4bd6cb982113d0331c080
Sayeem2004 <MohammadSSKC@gmail.com>
2021-12-07 09:07:44 -0500

    D07

--- 2021/D07/Python/Part1.py
+++ 2021/D07/Python/Part1.py
@@ -3,9 +3,15 @@ fout = open("../Part1.out", "w");
 
 def main():
     lines = [line.strip() for line in fin.read().split("\n")[0:-1]];
+    lines = [int(x) for x in lines[0].split(",")];
     fout.write(str(solve(lines)));
 
 def solve(lines):
-
-
+    lines.sort();
+    med = lines[len(lines)/2];
+    ans = 0;
+    for x in lines:
+        ans += abs(x-med);
+    return ans;
+    
 main();
