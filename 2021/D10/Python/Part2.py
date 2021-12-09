81a6aa2bc883646f13b1e2f4b0eda2e26c24a0f8
Sayeem2004 <MohammadSSKC@gmail.com>
2021-12-09 10:40:55 -0500

    restructure

--- 2021/D10/Python/Part2.py
+++ 2021/D10/Python/Part2.py
@@ -1,11 +1,9 @@
-fin = open("../input.in", "r");
-fout = open("../part2.out", "w");
-
 def main():
+    fin = open("../input.in", "r");
+    fout = open("../part2.out", "w");
     lines = [line.strip() for line in fin.read().split("\n")[0:-1]];
     fout.write(str(solve(lines)));
 
 def solve(lines):
-
 
 main();
---
+++ ACodeBank.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll int64_t
+
+void solve() {
+
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    freopen("../input.in","r",stdin);
+    freopen("../part1.out","w",stdout);
+
+}
