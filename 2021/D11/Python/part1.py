9cd573f68df3023de497ebc41732a9258a5aeca2
Sayeem2004 <MohammadSSKC@gmail.com>
2021-12-10 08:39:33 -0500

    Restructure

--- 2021/D11/Python/part1.py
+++
@@ -1,9 +0,0 @@
-def main():
-    fin = open("../input.in", "r");
-    fout = open("../part1.out", "w");
-    lines = [line.strip() for line in fin.read().split("\n")[0:-1]];
-    fout.write(str(solve(lines)));
-
-def solve(lines):
-
-main();
