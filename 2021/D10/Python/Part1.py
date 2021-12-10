9c965a46c9cae36a3f265934b923a96f170461f9
Sayeem2004 <MohammadSSKC@gmail.com>
2021-12-10 08:34:25 -0500

    D10

--- 2021/D10/Python/Part1.py
+++ 2021/D10/Python/Part1.py
@@ -5,5 +5,21 @@ def main():
     fout.write(str(solve(lines)));
 
 def solve(lines):
+    dict = {")": 3, "]": 57, "}": 1197, ">": 25137};
+    cnt = 0;
+    for line in lines:
+        lt = [];
+        for i,c in enumerate(line):
+            if (c in "([{<"): lt.append(c);
+            else:
+                add = False;
+                if (len(lt) == 0): add = True;
+                elif (c == ")" and lt[-1] != "("): add = True;
+                elif (c == "]" and lt[-1] != "["): add = True;
+                elif (c == "}" and lt[-1] != "{"): add = True;
+                elif (c == ">" and lt[-1] != "<"): add = True;
+                if (add == True): cnt += dict[c]; break;
+                else: lt.pop();
+    return cnt;
 
 main();
