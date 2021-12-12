2e9c32fdffa27987fdcea45a59999db0110b6e3a
Sayeem2004 <MohammadSSKC@gmail.com>
2021-12-12 01:12:54 -0500

    D12

--- 2021/D12/Python/part1.py
+++ 2021/D12/Python/part1.py
@@ -1,9 +1,45 @@
 def main():
-    fin = open("../input.in", "r");
-    fout = open("../part1.out", "w");
-    lines = [line.strip() for line in fin.read().split("\n")[0:-1]];
-    fout.write(str(solve(lines)));
+    fin = open("../input.in", "r")
+    fout = open("../part1.out", "w")
+    lines = [line.strip() for line in fin.read().split("\n")[0:-1]]
+    lines = [line.split("-") for line in lines]
+    fout.write(str(solve(lines)))
+
+
+def dfs(adj, visited, curr):
+    if (curr == "end"):
+        return 1
+    sum = 0
+    for node in adj[curr]:
+        if (node.islower()):
+            if (visited[node]):
+                continue
+            else:
+                visited[node] = True
+                sum += dfs(adj, visited, node)
+                visited[node] = False
+        else:
+            visited[node] = True
+            sum += dfs(adj, visited, node)
+            visited[node] = False
+    return sum
+
 
 def solve(lines):
+    adj, visited = {}, {}
+    for line in lines:
+        if (line[0] in adj):
+            adj[line[0]].append(line[1])
+        else:
+            adj[line[0]] = [line[1]]
+        if (line[1] in adj):
+            adj[line[1]].append(line[0])
+        else:
+            adj[line[1]] = [line[0]]
+        visited[line[0]] = False
+        visited[line[1]] = False
+    visited["start"] = True
+    return dfs(adj, visited, "start")
 
-main();
+
+main()
--- 2021/D12/Python/part2.py
+++ 2021/D12/Python/part2.py
@@ -1,9 +1,58 @@
 def main():
-    fin = open("../input.in", "r");
-    fout = open("../part2.out", "w");
-    lines = [line.strip() for line in fin.read().split("\n")[0:-1]];
-    fout.write(str(solve(lines)));
+    fin = open("../input.in", "r")
+    fout = open("../part2.out", "w")
+    lines = [line.strip() for line in fin.read().split("\n")[0:-1]]
+    lines = [line.split("-") for line in lines]
+    fout.write(str(solve(lines)))
+
+
+def dfs(adj, visited, curr, double):
+    if (curr == "end"):
+        if (double != "none"):
+            if (visited[double] == 2):
+                return 1
+            else:
+                return 0
+        else:
+            return 1
+    sum = 0
+    for node in adj[curr]:
+        if (node.islower()):
+            if (node == double and visited[node] < 2):
+                visited[node] += 1
+                sum += dfs(adj, visited, node, double)
+                visited[node] -= 1
+            elif (visited[node] == 0):
+                visited[node] += 1
+                sum += dfs(adj, visited, node, double)
+                visited[node] -= 1
+        else:
+            visited[node] += 1
+            sum += dfs(adj, visited, node, double)
+            visited[node] -= 1
+    return sum
+
 
 def solve(lines):
+    adj, visited = {}, {}
+    for line in lines:
+        if (line[0] in adj):
+            adj[line[0]].append(line[1])
+        else:
+            adj[line[0]] = [line[1]]
+        if (line[1] in adj):
+            adj[line[1]].append(line[0])
+        else:
+            adj[line[1]] = [line[0]]
+        visited[line[0]] = 0
+        visited[line[1]] = 0
+    ans = 0
+    visited["start"] = 1
+    for node in adj.keys():
+        if (node.islower() and node != "start" and node != "end"):
+            ans += dfs(adj, visited, "start", node)
+    ans += dfs(adj, visited, "start", "none")
+    return ans
 
-main();
+
+main()
