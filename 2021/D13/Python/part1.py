2c822837a39c8579c641e85e221f6e5943b12dc7
Sayeem2004 <MohammadSSKC@gmail.com>
2021-12-13 09:24:15 -0500

    D13

--- 2021/D13/Python/part1.py
+++ 2021/D13/Python/part1.py
@@ -1,9 +1,27 @@
 def main():
-    fin = open("../input.in", "r");
-    fout = open("../part1.out", "w");
-    lines = [line.strip() for line in fin.read().split("\n")[0:-1]];
-    fout.write(str(solve(lines)));
+    fin = open("../input.in", "r")
+    fout = open("../part1.out", "w")
+    lines = [line.strip() for line in fin.read().split("\n\n")]
+    nums = [[int(y) for y in x.split(",")] for x in lines[0].split("\n")]
+    folds = [[int(y) if (q == 1) else y for q, y in enumerate(x.split("="))] for x in lines[1].split("\n")]
+    fout.write(str(solve(nums, folds)))
 
-def solve(lines):
 
-main();
+def solve(nums, folds):
+    for fold in folds:
+        if (fold[0][-1] == "x"):
+            for num in nums:
+                if (num[0] > fold[1]):
+                    num[0] = fold[1]-(num[0]-fold[1])
+        else:
+            for num in nums:
+                if (num[1] > fold[1]):
+                    num[1] = fold[1]-(num[1]-fold[1])
+        break
+    lt = set()
+    for num in nums:
+        lt.add(",".join([str(x) for x in num]))
+    return len(lt)
+
+
+main()
--- 2021/D13/Python/part2.py
+++ 2021/D13/Python/part2.py
@@ -1,9 +1,31 @@
 def main():
-    fin = open("../input.in", "r");
-    fout = open("../part2.out", "w");
-    lines = [line.strip() for line in fin.read().split("\n")[0:-1]];
-    fout.write(str(solve(lines)));
+    fin = open("../input.in", "r")
+    fout = open("../part2.out", "w")
+    lines = [line.strip() for line in fin.read().split("\n\n")]
+    nums = [[int(y) for y in x.split(",")] for x in lines[0].split("\n")]
+    folds = [[int(y) if (q == 1) else y for q, y in enumerate(x.split("="))] for x in lines[1].split("\n")]
+    ret = solve(nums, folds)
+    for i in range(len(ret[0])):
+        for q in range(len(ret)):
+            fout.write(ret[q][i])
+            fout.write(" ")
+        fout.write("\n")
 
-def solve(lines):
 
-main();
+def solve(nums, folds):
+    for fold in folds:
+        if (fold[0][-1] == "x"):
+            for num in nums:
+                if (num[0] > fold[1]):
+                    num[0] = fold[1]-(num[0]-fold[1])
+        else:
+            for num in nums:
+                if (num[1] > fold[1]):
+                    num[1] = fold[1]-(num[1]-fold[1])
+    lt = [[" " for i in range(6)] for q in range(40)]
+    for num in nums:
+        lt[num[0]][num[1]] = "#"
+    return lt
+
+
+main()
