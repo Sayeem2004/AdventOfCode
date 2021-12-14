5d0661f9d0bca7149f2ae817054fccc5fadbfc66
Sayeem2004 <MohammadSSKC@gmail.com>
2021-12-14 14:54:42 -0500

    D14

--- 2021/D14/Python/part1.py
+++ 2021/D14/Python/part1.py
@@ -1,9 +1,36 @@
 def main():
-    fin = open("../input.in", "r");
-    fout = open("../part1.out", "w");
-    lines = [line.strip() for line in fin.read().split("\n")[0:-1]];
-    fout.write(str(solve(lines)));
+    fin = open("../input.in", "r")
+    fout = open("../part1.out", "w")
+    lines = [line.strip() for line in fin.read().split("\n\n")]
+    lt = list(lines[0])
+    dict = {}
+    for line in lines[1].split("\n"):
+        temp = line.split(" -> ")
+        dict[temp[0]] = temp[1]
+    fout.write(str(solve(lt, dict)))
 
-def solve(lines):
 
-main();
+def solve(lt, dict):
+    for _ in range(10):
+        cpy = [lt[0]]
+        for i in range(1, len(lt)):
+            temp = [lt[i-1], lt[i]]
+            val = "".join(temp)
+            if (val in dict):
+                cpy.append(dict[val])
+            cpy.append(lt[i])
+        lt = cpy
+    freq = {}
+    for ch in lt:
+        if (ch in freq):
+            freq[ch] += 1
+        else:
+            freq[ch] = 1
+    mx, mn = 0, 1000000
+    for val in freq.keys():
+        mx = max(mx, freq[val])
+        mn = min(mn, freq[val])
+    return mx-mn
+
+
+main()
--- 2021/D14/Python/part2.py
+++ 2021/D14/Python/part2.py
@@ -1,9 +1,54 @@
 def main():
-    fin = open("../input.in", "r");
-    fout = open("../part2.out", "w");
-    lines = [line.strip() for line in fin.read().split("\n")[0:-1]];
-    fout.write(str(solve(lines)));
+    fin = open("../input.in", "r")
+    fout = open("../part2.out", "w")
+    lines = [line.strip() for line in fin.read().split("\n\n")]
+    lt = list(lines[0])
+    freq = {}
+    for i in range(1, len(lt)):
+        tmp = lt[i-1]+lt[i]
+        if (tmp in freq):
+            freq[tmp] += 1
+        else:
+            freq[tmp] = 1
+    dict = {}
+    for line in lines[1].split("\n"):
+        temp = line.split(" -> ")
+        dict[temp[0]] = [temp[0][0]+temp[1], temp[1]+temp[0][1]]
+    fout.write(str(solve("".join(lt), freq, dict)))
 
-def solve(lines):
 
-main();
+def solve(initial, freq, dict):
+    for _ in range(40):
+        cpy = {}
+        for f in freq.keys():
+            temp = dict[f]
+            if (temp[0] in cpy):
+                cpy[temp[0]] += freq[f]
+            else:
+                cpy[temp[0]] = freq[f]
+            if (temp[1] in cpy):
+                cpy[temp[1]] += freq[f]
+            else:
+                cpy[temp[1]] = freq[f]
+        freq = cpy
+    ret = {}
+    for f in freq.keys():
+        if (f[0] in ret):
+            ret[f[0]] += freq[f]
+        else:
+            ret[f[0]] = freq[f]
+        if (f[1] in ret):
+            ret[f[1]] += freq[f]
+        else:
+            ret[f[1]] = freq[f]
+    ret[initial[0]] += 1
+    ret[initial[-1]] += 1
+    mx, mn = 0, 1000000000000000
+    for r in ret.keys():
+        ret[r] /= 2
+        mx = max(mx, ret[r])
+        mn = min(mn, ret[r])
+    return mx-mn
+
+
+main()
