b7c54022ef7e8c9ea618b50be1451592f0a9fa44
Sayeem2004 <MohammadSSKC@gmail.com>
2021-12-17 10:23:05 -0500

    D17

--- ACodeBank.py
+++ ACodeBank.py
@@ -1,12 +1,9 @@
 def main():
-    fin = open("../input.in", "r")
-    fout = open("../part.out", "w")
+    fin, fout = open("../input.in", "r"), open("../part.out", "w")
     lines = [line.strip() for line in fin.read().split("\n")[0:-1]]
     fout.write(str(solve(lines)))
 
-
 def solve(lines):
     return lines
-
 
 main()
