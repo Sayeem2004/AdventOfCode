6ff59ba497ca32edabd5f6e911bab80d620a6c11
Sayeem2004 <MohammadSSKC@gmail.com>
2022-01-24 14:44:02 -0500

    add new files

--- 2021/D18/Python/part1.py
+++ 2021/D18/Python/part1.py
@@ -1,9 +1,42 @@
 def main():
     fin, fout = open("../input.in", "r"), open("../part1.out", "w")
-    lines = [line.strip() for line in fin.read().split("\n")[0:-1]]
+    lines = [eval(line.strip()) for line in fin.read().split("\n")[0:-1]]
     fout.write(str(solve(lines)))
 
-def solve(lines):
-    return lines
+def check(pair, depth):
+    if (isinstance(pair, int)): return (pair >= 10)
+    else: return (check(pair[0], depth+1) or check(pair[1], depth+1) or depth >= 4)
 
+def add(pair, val, mode):
+    if (isinstance(pair, int)):
+        pair += val
+        return pair
+    else:
+        if (mode == "r"): return [pair[0], add(pair[1], val, "r")]
+        if (mode == "l"): return [add(pair[0], val, "l"), pair[1]]
+
+def simplify(pair, depth):
+    if (not isinstance(pair, int)):
+        sl, sr = 0, 0
+        while (check(pair[0], depth+1)):
+            [pair[0], l1, r1] = simplify(pair[0], depth+1)
+            pair[1] = add(pair[1], r1, "l")
+            if (l1 != 0): return [pair, l1, 0]
+        while (check(pair[1], depth+1)):
+            [pair[1], l2, r2] = simplify(pair[1], depth+1)
+            pair[0] = add(pair[0], l2, "r")
+            if (r2 != 0): return [pair, 0, r2]
+        if (depth >= 4): return [0, sl+pair[0], sr+pair[1]]
+        else: return [pair, sl, sr]
+    else:
+        if (pair >= 10): return [[(pair-1)//2, (pair+1)//2], 0, 0]
+        else: return [pair, 0, 0]
+
+def solve(lines):
+    curr = lines[0]
+    for line in lines[1:]:
+        curr = [curr, line]
+        while (check(curr, 0)):
+            [curr, l, r] = simplify([curr, line], 0)
+    return curr
 main()
--- 2021/D20/Python/part1.py
+++ 2021/D20/Python/part1.py
@@ -1,9 +1,36 @@
 def main():
     fin, fout = open("../input.in", "r"), open("../part1.out", "w")
-    lines = [line.strip() for line in fin.read().split("\n")[0:-1]]
-    fout.write(str(solve(lines)))
+    lines = [line.strip() for line in fin.read().split("\n\n")]
+    key = list(lines[0])
+    img = [list(x) for x in lines[1].split("\n")]
+    fout.write(str(solve(img, key)))
 
-def solve(lines):
-    return lines
+def enhance(img, key):
+    ret = [["." for x in range(len(img[0])+2)] for y in range(len(img)+2)]
+    for y in range(0, len(ret)):
+        for x in range(0, len(ret[0])):
+            val = ""
+            for dy in range(-1, 2):
+                for dx in range(-1, 2):
+                    ny = y+dy
+                    if (ny < 1 or ny >= len(ret)-1):
+                        val += "0"
+                        continue
+                    nx = x+dx
+                    if (nx < 1 or nx >= len(ret[0])-1):
+                        val += "0"
+                        continue
+                    val += "1" if img[ny-1][nx-1] == "#" else "0"
+            ret[y][x] = key[int(val, 2)]
+    return ret
+
+def solve(img, key):
+    for i in range(2):
+        img = enhance(img, key)
+    cnt = 0
+    for i in range(len(img)):
+        for q in range(len(img[0])):
+            cnt += 1 if img[i][q] == "#" else 0
+    return cnt
 
 main()
